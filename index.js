b6109a284104bf0b0935e3d8b5cc0053f0ad6e47
samk <samuelkariuki1@student.moringaschool.com>
2023-06-17 12:36:14 +0300

    Done with Array assingment

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-// Write your solution here!
-cat
+// Write your solution 
+ const cats = ["Milo", "Otis", "Garfield"]
+ function destructivelyAppendCat (names){
+    return cats.push("Ralph")
+ }
+function destructivelyPrependCat (names){
+    return cats.unshift("Bob")
+}
+    
+function destructivelyRemoveLastCat (names){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat (names){
+    return cats.shift()
+}
+
+function appendCat (names){
+    appendCat = [...cats, "Broom"]
+    return appendCat
+
+}
+function prependCat (name){
+    prependCat = ["Arnold", ...cats]
+    return prependCat
+}
+ function removeLastCat (names){
+    return cats.slice(0, -1)
+ }
+
+ function removeFirstCat (names){
+    return cats.slice(1)
+ }
+
